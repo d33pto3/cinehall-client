ddbaf339240f44ed6dba3742d073572021cb8bf5
d33pto3 <rakinfrhn@gmail.com>
2025-03-07 01:59:18 +0600

    monorepo setup using turborepo

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-3xl font-bold underline">
+          Welcome to the Admin App!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
