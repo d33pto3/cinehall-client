c2d8562cb68a93b647f2d5646f9fe92f8b3591fd
d33pto3 <rakinfrhn@gmail.com>
2025-03-06 11:56:22 +0600

    Removed all existing files

--- src/routes/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children, ...props }) => {
-  const { token } = JSON.parse(localStorage.getItem("user"));
-
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
