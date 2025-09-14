0dca47c67f98040920eb220253e35a80335a9fc3
d33pto3 <rakinfrhn@gmail.com>
2025-09-14 18:06:54 +0600

    feat: add hallowner screens management components
    - Implemented DeleteButton for deleting screens with confirmation dialog.
    - Created HallownerScreensTable for displaying and managing screens.
    - Developed ListOfHallownerScreens to fetch and display screens with filters and pagination.
    - Added ScreenDialog component for reusable dialog functionality.
    - Introduced ScreenMoreAction for managing screen actions (view/delete).
    - Created ScreensFilter for filtering screens based on various criteria.
    - Implemented SearchActivity for debounced search functionality.
    - Developed ViewButton for viewing screen details.
    - Added delete-screen-form and view-screen-form for handling respective actions.
    - Updated hallowner screen index page to integrate new components.

---
+++ apps/admin/src/components/hallowner/screens/ScreenMoreAction.tsx
@@ -0,0 +1,26 @@
+// import { DeleteButton } from "./DeleteScreenButton";
+// import { ViewButton } from "./ViewButton";
+import MoreAction from "@/components/common/MoreActions";
+import { ViewButton } from "./ViewButton";
+import { DeleteButton } from "./DeleteScreenButton";
+
+export default function ScreenMoreAction({
+  screenId,
+  onDeleted,
+}: {
+  screenId: string;
+  onDeleted?: () => void;
+}) {
+  // Define the actions for the reusable MoreAction component
+  const actions = [
+    {
+      component: <ViewButton screenId={screenId} />,
+    },
+    {
+      component: <DeleteButton screenId={screenId} onDeleted={onDeleted} />,
+      isSeparatorBefore: true,
+    },
+  ];
+
+  return <MoreAction actions={actions} />;
+}
---
+++ apps/admin/src/components/hallowner/screens/ViewButton.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { MdViewList } from "react-icons/md";
+import { ScreenDialog } from "./ScreenDialog";
+import ViewScreenForm from "./view-screen-form";
+// import ViewListingForm from "@/components/more-actions-forms/view-listing-form";
+// import { ListingDialog } from "./ListingDialog";
+
+export function ViewButton({ screenId }: { screenId: string }) {
+  const [isViewOpen, setIsViewOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsViewOpen(true);
+        }}
+        className="w-full text-left flex items-center gap-2 cursor-pointer"
+      >
+        <MdViewList />
+        <span>View</span>
+      </button>
+      <ScreenDialog
+        isOpen={isViewOpen}
+        setIsOpen={setIsViewOpen}
+        title="View & Edit Screen"
+        description="Click to view the Screen description"
+      >
+        <ViewScreenForm screenId={screenId} setIsOpen={setIsViewOpen} />
+      </ScreenDialog>
+    </>
+  );
+}
